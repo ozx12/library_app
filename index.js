2a2998d4af2540e7226e0b007bd5bbf74d2bab5e
ozx12 <sergey@butusov.su>
2022-11-13 23:47:19 +0300

    library_app_0.2.2 final

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const express = require('express');
 const booksRouter = require('./routes/api/books');
 const userRouter = require('./routes/api/user');
 const books = require('./DB/books');
+var fs = require('fs');
 
+fs.stat('public/img', function(err) {
+    if (!err) {
+        console.log('папка для изображений public/img');
+    }
+    else if (err.code == 'ENOENT') {         
+        fs.mkdir('public/img', { recursive: true }, err => {
+           if(err) throw err; // не удалось создать папки
+           console.log('папка для изображений успешно создана public/img');
+        });
+    }
+});
 
 const app = express();
 app.use(express.json());
