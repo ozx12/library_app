65e4501426e8d3badd617c7d5d84be30ae4f3b5c
ozx12 <sergey@butusov.su>
2022-11-13 19:50:39 +0300

    0.2.2 Middleware

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const { v4: uuid } = require('uuid');
 
 
 class Book {
-    constructor(title = "", description = "", authors = "", favorite = "", fileCover = "", fileName = "", id = uuid()) {
+    constructor(title = "", description = "", authors = "", favorite = "", fileCover = "", fileName = "", fileBook = "", id = uuid()) {
         this.title = title;
         this.description = description;
         this.authors = authors;
         this.favorite = favorite;
         this.fileCover = fileCover;
         this.fileName = fileName;
-        this.id = id;
+        this.fileBook = fileBook;
+        this.id = id;       
     }
 };
 
